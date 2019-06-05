c24c5b9474bdbdfa328fe021bf1a268213722287
IvanBabic26 <ivanbabic26@gmail.com>
2019-06-05 14:30:32 +0200

    "meal plan page with calorie calculator"

--- src/components/BMICalculator/BMICalculator.js
+++ src/components/BMICalculator/BMICalculator.js
@@ -66,56 +66,91 @@ class BMICalculator extends Component {
 
   render() {
     return (
-      <div className="bmicalc">
-        <div className="bmicalcHeader">
-          <h2>BMI Calculator</h2>
-        </div>
-        <form id="wrapper" onSubmit={this.submitForm}>
-          <label>Please enter your name:</label>
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onBlur={this.blur}
-            onChange={this.change}
-          />
-          <label>Enter your age:</label>
-          <input
-            type="text"
-            name="age"
-            value={this.state.age}
-            onBlur={this.blur}
-          />
-          <label>Sex:</label>
-          <select className="selectOption">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-          <label>Enter your height in cm:</label>
-          <input
-            type="text"
-            name="height"
-            value={this.state.height}
-            onBlur={this.blur}
-            onChange={this.heightChange}
-          />
-          <label>Enter your weight in kg:</label>
-          <input
-            type="text"
-            name="weight"
-            value={this.state.weight}
-            onChange={this.weightChange}
-          />
-          <label>Don't be scared!</label>
-          <input type="submit" className="btnSubmit" value="Submit" />
-        </form>
-        {this.state.formComplete && (
-          <div className="outputbmi">
-            {`Hello ${this.state.name}! Your BMI is currently `}
-            {this.state.bmi}!{this.state.message}
-            {this.state.optimalweight}.
+      <div className="bmiCalc">
+        <div className="bmiPage">
+          <div className="bmiCalcWrapper">
+            <div className="bmiCalcHeader">
+              <h2>BMI Calculator</h2>
+            </div>
+            <form id="formBmi" onSubmit={this.submitForm}>
+              <label>Please enter your name:</label>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onBlur={this.blur}
+                onChange={this.change}
+              />
+              <label>Enter your age:</label>
+              <input
+                type="text"
+                name="age"
+                value={this.state.age}
+                onBlur={this.blur}
+              />
+              <label>Sex:</label>
+              <select className="selectOption">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+              <label>Enter your height in cm:</label>
+              <input
+                type="text"
+                name="height"
+                value={this.state.height}
+                onBlur={this.blur}
+                onChange={this.heightChange}
+              />
+              <label>Enter your weight in kg:</label>
+              <input
+                type="text"
+                name="weight"
+                value={this.state.weight}
+                onChange={this.weightChange}
+              />
+              <label>Don't be scared!</label>
+              <input type="submit" className="btnSubmit" value="Submit" />
+            </form>
           </div>
-        )}
+        </div>
+        <div className="bmiTextWrapper">
+          <h1 className="bmiTextHeader">Body Mass Index</h1>
+
+          <div className="bmiIntro">
+            <h2 className="secondHeaderText">
+              The BMI calculator is a useful tool that measures whether you are
+              overweight, underweight, or just right. Your weight alone is not
+              enough to tell, as a tall, skinny man may easily weigh more than a
+              short but rotund woman. The body mass index, or BMI, overcomes
+              this problem by finding a ratio of your weight to your height, and
+              returning a single number. This number will fit into a category on
+              the scale of BMI ranges, which are defined as underweight, normal,
+              overweight, and obese.
+            </h2>
+            <h3 className="thirdHeaderText">
+              At the left you can see our BMI calculator, there are fields for
+              you to plug in your height and weight plus some other stuff. Plug
+              in these values, and you'll instantly have your BMI and a little
+              message telling you what range you fall into. Our BMI calculator
+              makes it a piece of cake for you to find your number. However,
+              please remember that BMI is a rough estimation. The result can be
+              somewhat misleading for individuals who are well muscled (such as
+              body builders), or for those who have lost a significant amount of
+              muscle (such as the elderly).
+            </h3>
+          </div>
+          <h2 className="outputHeader">
+            Use the BMI calculator to see your Body Mass Index Ratio. The
+            results are shown here:
+          </h2>
+          {this.state.formComplete && (
+            <div className="outputBmi">
+              {`Hello ${this.state.name}! Your BMI is currently `}
+              {this.state.bmi}!{this.state.message}
+              {this.state.optimalweight}.
+            </div>
+          )}
+        </div>
       </div>
     );
   }
